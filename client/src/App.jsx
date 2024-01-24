c0131517b21b8b71ecd578f37521425616014f1f
NAVEEN K <naveenkkn03@gmail.com>
2024-01-24 16:06:05 +0530

    private route will be added dashboard protected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,11 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
-import { Button } from 'flowbite-react';
+import { Button } from "flowbite-react";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import FooterComponent from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
   return (
     <BrowserRouter>
@@ -19,7 +20,9 @@ const App = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
       <FooterComponent />
     </BrowserRouter>
