dc7d516c4469c23c2d13e67e02bb568285db5e6a
NAVEEN K <naveenkkn03@gmail.com>
2024-01-25 18:03:10 +0530

    get post route is completed

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -2,11 +2,12 @@ import express from 'express';
 import { isAdmin, verifyAuthToken } from '../middleware/verifyAuthToken.js';
 import joiValidation from '../middleware/joiValidation.js';
 import {postValidationSchema} from '../validations/post.validation.js'
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create-post',verifyAuthToken,isAdmin,joiValidation(postValidationSchema),createPost)
+router.get('/get-post',getPost)
 
 
 export default router;
