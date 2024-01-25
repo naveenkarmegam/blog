d2b317a1d756a95753e8f9dafae4a666f940f6e3
NAVEEN K <naveenkkn03@gmail.com>
2024-01-25 08:47:24 +0530

    delete user api route completed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,3 +52,15 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      return next(customError(403, "You are not allowed to delete this user"));
+    }
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "your account successfully deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,10 +2,12 @@ import express from "express";
 import verifyAuthToken from "../middleware/verifyAuthToken.js";
 import { updateValidationSchema } from "../validations/user.validation.js";
 import joiValidation from "../middleware/joiValidation.js";
-import { updateUser } from "../controllers/user.controller.js";
+import { deleteUser, updateUser } from "../controllers/user.controller.js";
 
 
 const router = express.Router();
 
-router.put('/update/:id',verifyAuthToken,updateUser)
+router.put('/update/:id',verifyAuthToken,updateUser);
+router.delete('/delete/:id',verifyAuthToken,deleteUser);
+
 export default router;
