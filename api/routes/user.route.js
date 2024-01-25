f80ea374d9385efb200ea63127e98196fbdf9f83
NAVEEN K <naveenkkn03@gmail.com>
2024-01-25 10:44:10 +0530

    sign out route completed

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,12 +2,13 @@ import express from "express";
 import verifyAuthToken from "../middleware/verifyAuthToken.js";
 import { updateValidationSchema } from "../validations/user.validation.js";
 import joiValidation from "../middleware/joiValidation.js";
-import { deleteUser, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, signOut, updateUser } from "../controllers/user.controller.js";
 
 
 const router = express.Router();
 
 router.put('/update/:id',verifyAuthToken,updateUser);
 router.delete('/delete/:id',verifyAuthToken,deleteUser);
+router.post('/sign-out',signOut);
 
 export default router;
