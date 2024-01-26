26e8a5f79ac76211c09c9df86f6705890550dca9
NAVEEN K <naveenkkn03@gmail.com>
2024-01-26 18:24:52 +0530

    call to action component is added

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -62,9 +63,13 @@ const PostPage = () => {
           {post && (post.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
-      <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
-
-    </div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
+      <div className="w-full mx-auto max-w-4xl">
+        <CallToAction />
+      </div>
     </main>
   );
 };
