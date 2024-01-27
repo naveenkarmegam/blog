8cb1bd6c594e4c9ca3f329a4887e7da2c025d521
NAVEEN K <naveenkkn03@gmail.com>
2024-01-27 11:43:03 +0530

    likes functionality fully update for comment

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -3,9 +3,11 @@ import { verifyAuthToken, isAdmin } from "../middleware/verifyAuthToken.js";
 import {
   createComment,
   getComment,
+  likeComment,
 } from "../controllers/comment.controller.js";
 const router = express.Router();
 
 router.post("/create", verifyAuthToken, createComment);
 router.get("/get-post-comments/:postId", getComment);
+router.put("/likeComment/:commentId", verifyAuthToken, likeComment);
 export default router;
